39aa99ff7745962b95b17cb581ca3220df118a21
rmoncada21 <106123923+rmoncada21@users.noreply.github.com>
2023-07-04 20:38:58 -0600

    pimeros ejercicios del curso de ATS C++ y ejemplos de arreglos

---
+++ ATS/2-tipos_de_datos.cpp
@@ -0,0 +1,19 @@
+//Tipos de datos básicos en c++
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numero = 15;
+    float flotante = 15.5; 
+    double doble = 26.3666;
+    char caracter = 'a';
+
+    cout<<numero<<endl;
+    cout<<flotante<<endl;
+    cout<<doble<<endl;
+    cout<<caracter<<endl;
+
+    return 0;
+}
