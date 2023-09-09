799e97f6b0a2fa84aaa8765d2ec65e3d58b0cbae
rmoncada21 <106123923+rmoncada21@users.noreply.github.com>
2023-09-08 18:14:27 -0600

    add .c

--- Entrada_y_Salida/2.Indice_masa_coporal.c
+++ Entrada_y_Salida/2.Indice_masa_coporal.c
@@ -1,24 +1,20 @@
+//copyright
+
 #include <stdio.h> //biblioteca estándar de C
 #include <stdbool.h>
 #include <assert.h> //define la macrodepuración
 
 
-/* pseudocódigo
+bool classify_person(void);
+bool is_mass_valid(double mass);
+bool is_height_valid(double height);
+const char* class_body_mass_index(double bmi);
 
-Read weight in kilograms 
-Read height in centimeters
+// procedure Main:
+//   While there is data do
+//     Classify person
 
-If is weight and height is valid
-    Create bmi as weight / height^2
-
-Else 
-    print invaldid data
-    .
-    .
-    . seguir pseudocódigo
-
- */
-
+// procedure Classify person:
 
 int main(void){
     /* FILE* file = fopen("nombre_arhivo.txt", "r");   //forma de abrir un archivo
@@ -31,33 +27,28 @@ int main(void){
     return 0;
 }
 
-bool classify_person(void);
-
-// procedure Main:
-//   While there is data do
-//     Classify person
-
-// procedure Classify person:
 bool classify_person(void) {
     // Read mass in kilograms
     double mass = 0.0;
     // Read height in centimeters
-    double height = 0.0;
-    if (scanf("%lg %lg", &mass, &height) == 2) {
+    double height_cm = 0.0;
+    if (scanf("%lg %lg", &mass, &height_cm) == 2) {
     // Print mass, height
-        printf("%.2lf %.2lf", mass, height);
+        printf("%6.2lf %6.2lf ", mass, height_cm);
         // If mass is valflid and height is valid then
-        if (is_mass_valid(mass) && is_height_valid(height)) {
+        if (is_mass_valid(mass) && is_height_valid(height_cm)) {
+            
+            const double height_meters = height_cm / 100.0;
             // Create body mass index as mass / height^2
-            const double bmi = mass / (height * height);
+            double bmi = mass / (height_meters * height_meters);
             // Create nutritional state as the result of classifying the body mass index
             // no modifica ni el puntero no la constante
             const char* const nutrition_state = class_body_mass_index(bmi);
             // Print body mass index, nutritional state
-            printf("%.2lf %s\n", bmi, nutrition_state);
+            printf("%5.2lf %s\n", bmi, nutrition_state);
         }else{
         // Print invalid data
-        printf("invalid data \n")
+        printf("invalid data \n");
     }
     //continue readinf the next person
     return true;
@@ -83,18 +74,18 @@ bool is_height_valid (double height) {
 // function Classify the body mass index (bmi):
 const char* class_body_mass_index(double bmi) {
 //   If bmi < 18.5 then
-    if (bmi < 18.5 then) {
-        return "underwight"
+    if (bmi < 18.5) {
+        return "underwight";
     }
 //     return "underweight"
 
 //   If bmi < 25 then
     if( bmi < 25 ){ 
-        return "normal"
+        return "normal";
     }
 //   If bmi < 30 then
     if (bmi < 30) {
-        return "overweight"
+        return "overweight";
     }
 //     return "overweight"
 //   return "obese"
