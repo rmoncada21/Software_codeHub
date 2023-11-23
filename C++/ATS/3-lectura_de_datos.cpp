fda518c15581ed955a0c5616a8bf35674612e70c
rmoncada21 <106123923+rmoncada21@users.noreply.github.com>
2023-11-22 23:22:40 -0600

    Carpeta para ejemplos C++

---
+++ C++/ATS/3-lectura_de_datos.cpp
@@ -0,0 +1,17 @@
+//Lectura o entrada de datos
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numero;
+
+    cout<<"Digite un numero: ";
+    cin>>numero;
+
+    cout<<"El dato ingresado fue: "<<numero<<endl;
+    
+    return 0;
+}
