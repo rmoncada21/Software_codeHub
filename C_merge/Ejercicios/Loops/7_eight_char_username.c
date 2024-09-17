9ecf5b44a137ff654729a135ed5b3ca8586010f9
RomaF40 <106123923+rmoncada21@users.noreply.github.com>
2024-09-17 17:50:02 -0600

    Add 7_eight_char_username.c file.

--- C_merge/Ejercicios/Loops/7_eight_char_username.c
+++ C_merge/Ejercicios/Loops/7_eight_char_username.c
@@ -4,13 +4,34 @@ int valid_username(char[20]);
 
 int main(){
     char usuario[20];
-    
+
     printf("Digite un nombre de usuario válido \n");
     scanf("%s", usuario);
-
     valid_username(usuario);
+    // int pos = 2;
+    // printf("Valor usuario[pos]=%c \n", usuario[pos]);
+
+    while(1){
+        printf("Digite un nombre de usuario válido \n");
+        scanf("%s", usuario);
+        
+        if(valid_username(usuario)>8){
+            printf("Nombre de usuario válido \n");
+            break;
+        }
+    }
 }
 
-int valid_username(char[20]){
-    
+int valid_username(char usuario[20]){
+    int contador = 0;
+    /* 
+        Encontrar el caracter nulo '\0' de la cadena usuario
+        Es importante usar comillas simples en los caracteress
+    */
+    while(usuario[contador]!='\0'){
+        contador++;
+    }
+    printf("contador: %i \n", contador);
+
+    return contador;
 }
