1736971b1e069d32d0b6b93502447e3b50c08754
RomaF41 <106123923+rmoncada21@users.noreply.github.com>
2025-01-12 21:57:57 -0600

    C Arrays Exercise 13 done: Insert a new value in a sorted array

---
+++ C_merge/Ejercicios/Arreglos/13_insert_value.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void get_array(int*, int);
+void insert_value(int*, int);
+void printf_array(int*, int);
+void sort_array(int*, int);
+
+void get_array(int* arreglo, int size){
+    printf("\nTamaño: %i", size);
+    int valor=0;
+
+    for(int i=0; i<size; i++){
+        printf("Ingrese un valor: \n");
+        scanf("%i", &valor);
+        *(arreglo+i)=valor;
+    }
+
+    printf_array(arreglo, size);
+}
+
+void printf_array(int* arreglo, int size){
+    printf("\n");
+    for(int i=0; i<size; i++){
+        printf("%i ", *(arreglo+i));
+    }
+}
+
+void sort_array(int* arreglo, int size){
+    int swap;
+
+    while(swap){
+        swap=0;
+        for(int i=0; i<size; i++){
+            if( i<(size-1) && arreglo[i]>arreglo[i+1] ){
+                int temporal=arreglo[i];
+                arreglo[i]=arreglo[i+1];
+                arreglo[i+1]=temporal;
+                swap=1;
+            }
+        }
+    }
+
+    printf_array(arreglo, size);
+}
+
+void insert_value(int* arreglo, int size){
+
+}
+
+int main(void){
+    
+}
--- C_merge/Ejercicios/Arreglos/Enunciados_arreglos.md
+++ C_merge/Ejercicios/Arreglos/Enunciados_arreglos.md
@@ -70,3 +70,20 @@ element - 4 : 9
 Expected Output :
 Elements of array in sorted ascending order:
 2 4 5 7 9
+
+# 13. Write a program in C to insert the values in the array (sorted list).
+Test Data :
+Input number of elements you want to insert (max 100): 5
+Input 5 elements in the array in ascending order:
+element - 0 : 2
+element - 1 : 3
+element - 2 : 4
+element - 3 : 7
+element - 4 : 8
+Input the value to be inserted : 5
+The existing array list is :
+2 3 4 7 8
+After Insert the list is :
+2 3 4 5 7 8
+
+# 
